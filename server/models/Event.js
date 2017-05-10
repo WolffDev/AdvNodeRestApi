964600cb7855dd13cdb649e07d910328de997a87
David Wolff <info@davidwolff.dk>
2017-05-11 01:44:41 +0200

    added support for array objects in participants

--- server/models/Event.js
+++ server/models/Event.js
@@ -63,9 +63,10 @@ let Event = mongoose.model('Event', {
 		type: {DetailSchema},
 		required: true
 	},
-	participants: {
-		type: {ParticipantSchema}
-	}
+	participants: [{
+		type: ParticipantSchema
+		// http://stackoverflow.com/questions/19695058/how-to-define-object-in-array-in-mongoose-schema-correctly-with-2d-geo-index
+	}]
 });
 
 module.exports = {Event};
