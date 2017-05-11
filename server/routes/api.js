770c6a7ab2a840d7946462499a6aad631392e609
David Wolff <info@davidwolff.dk>
2017-05-11 11:52:55 +0200

    added eventList endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,6 +2,9 @@ const routes = require('express').Router();
 
 const event = require('./event.js');
 
+const eventList = require('./eventList.js');
+
 routes.use('/event', event);
+routes.use('/eventlist', eventList);
 
 module.exports = routes;
